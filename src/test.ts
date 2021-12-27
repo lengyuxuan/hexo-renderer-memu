d9b851d7e0f409393a2992a10bddba1dff823512
fangjin <fangjin.1227@foxmail.com>
2021-12-27 16:39:41 +0800

    update

--- src/test.ts
+++ src/test.ts
@@ -4,5 +4,5 @@ import render from './render';
 
 (async () => {
   const html = await render({ path: path.join(__dirname, '../README.md') });
-  fs.writeFileSync(path.join(__dirname, '../README.html'), html);
+  fs.writeFileSync(path.join(__dirname, '../dist/README.html'), html);
 })();
